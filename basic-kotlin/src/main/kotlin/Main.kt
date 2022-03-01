5a0cc9666354b569303175144ddcd640c385bc41
Grinaldi Wisnu <grinaldi.wisnu@mekari.com>
2022-03-02 03:28:55 +0700

    first commit

---
+++ basic-kotlin/src/main/kotlin/Main.kt
@@ -0,0 +1,18 @@
+fun palindrome(text: String): String {
+    return ""
+}
+
+fun parentheses(text: String): Boolean {
+    return false
+}
+
+fun myName(name: String?, age: Int?): String {
+    return ""
+}
+
+fun main(args: Array<String>) {
+    println(palindrome("kasur rusak"))
+    println("its " + parentheses("(())()"))
+    println(myName(null, null))
+    println(myName("Your name", 22))
+}
