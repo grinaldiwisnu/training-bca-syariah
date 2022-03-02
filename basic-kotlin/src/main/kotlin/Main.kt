9670f51e58e0dc9deda9058e1f45ed9db743cdbd
Grinaldi Wisnu <grinaldi.wisnu@mekari.com>
2022-03-02 20:18:15 +0700

    finishing submission for basic kotlin

--- basic-kotlin/src/main/kotlin/Main.kt
+++ basic-kotlin/src/main/kotlin/Main.kt
@@ -1,16 +1,45 @@
+import java.util.*
+
 fun palindrome(text: String): String {
-    return ""
+    var isPalindrome = true
+    var i = 0
+    while (i < text.length / 2) {
+        if (text[i] != text[text.length - 1 - i])
+            isPalindrome = false
+        i++
+    }
+
+    if (isPalindrome)
+        return "$text is palindrome"
+
+    return "$text is not palindrome"
 }
 
 fun parentheses(text: String): Boolean {
-    return false
+    val stack: Stack<Char> = Stack<Char>()
+    for (char in text) {
+        when (char) {
+            '(' -> stack.push(char)
+            ')' -> {
+                if (stack.size != 0)
+                    stack.pop()
+                else
+                    return false
+            }
+            else -> break
+        }
+    }
+    return stack.size == 0
 }
 
 fun myName(name: String?, age: Int?): String {
-    return ""
+    if (name.isNullOrEmpty() || age == null || age == 0)
+        return "Name or age was not defined"
+
+    return "$name now $age years old"
 }
 
-fun main(args: Array<String>) {
+fun main() {
     println(palindrome("kasur rusak"))
     println("its " + parentheses("(())()"))
     println(myName(null, null))
